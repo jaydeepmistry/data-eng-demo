e6526db0a85e1f91c34cd3d0483a0b5d82903b20
Jaydeep Mistry <jd.mistry@gmail.com>
2024-11-27 14:16:56 -0500

    update airflow init to include user creation

--- airflow/scripts/init.sh
+++ airflow/scripts/init.sh
@@ -3,6 +3,15 @@
 # Initialize the database
 airflow db init
 
+# Upgrade the database
+airflow db upgrade
+
+# Create Admin user, if it already exists it will not re-create it
+airflow users create --username admin --password admin --firstname admin --lastname admin --role Admin --email admin@example.com
+
+# Verify that Admin user exists
+airflow users list
+
 # Start the web server
 airflow webserver --port 8080 &
 
